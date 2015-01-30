2deac6ddd16f0cea04208b7544b76d31c404a1f8
Andrew <aribicki@gmail.com>
2015-01-30 15:45:55 -0500

    add comments #

--- Point.java
+++ Point.java
@@ -1,8 +1,17 @@
+/* Point class for cmp112
+	Andrew Ribicki
+	30 January 2015
+*/
 public class Point {
 	public double x,y;
-
+// Print out the coordinates for a single point
 	public void printPoint() {
 	  System.out.println("X coordinate: " +  this.x);
 	  System.out.println("Y coordinate: " +  this.y);
+	}
+// clear x and y values (i.e. reset point to 0
+	 public void clear() {
+	  x = 0.0;
+ 	  y = 0.0;
 	}
 }
--- PointDriver.java
+++ PointDriver.java
@@ -1,19 +1,27 @@
+/* Driver for Point class
+	Andrew Ribicki	
+	30 January 2015
+*/
 public class PointDriver {
 
 	public static void main(String[] args ) {
 	Point lowerLeft = new Point();
 	Point upperRight = new Point();
 	Point middlePoint = new Point();
-
+// create three points
 	lowerLeft.x = 0.0;
 	lowerLeft.y = 0.0;
-
+//give them some values
 	upperRight.x = 1280.0;
 	upperRight.y = 1024.0;
 
 	middlePoint.x = 640.0;
 	middlePoint.y = 512.0;
-
-	lowerLeft.printPoint();
+// print one of the points
+	upperRight.printPoint();
+// clear it
+	upperRight.clear();
+//print it again
+	upperRight.printPoint();
 	}
 }
